10e45e357c88bc239dc0430a833fcae7cca93235
Marcela Roig <ilkumquat@gmail.com>
2023-09-16 11:21:19 -0300

    Creación plantilla base y herencia en index Creación carpeta statics e incorporación de estilos.

--- core/views.py
+++ core/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index (request):
-    return HttpResponse("Hola Manola")
+    return render(request, "core/index.html")
