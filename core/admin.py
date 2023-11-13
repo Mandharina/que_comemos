8488b63f676c697762d2332d10b1f12651a248cf
Ale0602 <126637451+Ale0602@users.noreply.github.com>
2023-11-13 17:17:46 -0300

    Update admin.py
    He puesto los modelos al admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from core.models import Receta, IngredienteMedido, Ingredientes, Organizador_Semanal, Usuario, MiRecetario
 
 # Register your models here.
+
+admin.site.register(Receta)
+admin.site.register(IngredienteMedido)
+admin.site.register(Ingredientes)
+admin.site.register(Organizador_Semanal)
+admin.site.register(Usuario)
+admin.site.register(MiRecetario)
